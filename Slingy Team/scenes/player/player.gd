c7054f4f5854e357283e74f3e957fd3ff86cf78f
Alex Niunko <138675683+svindom@users.noreply.github.com>
2024-08-09 15:53:20 +0300

    fixed the player's animation (water salsh)

--- Slingy Team/scenes/player/player.gd
+++ Slingy Team/scenes/player/player.gd
@@ -3,13 +3,13 @@ extends RigidBody2D
 
 @onready var water_splash_animation: AnimatedSprite2D = $WaterSplashAnimation
 @onready var ball_sprite: Sprite2D = $BallSprite
-
+@onready var player_delete_timer: Timer = $PlayerDeleteTimer
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SignalManager.on_player_destroyed.connect(play_water_spalash_animation)
+	SignalManager.on_player_destroyed.connect(delete_player)
 	water_splash_animation.hide()
 
 
@@ -19,8 +19,19 @@ func _process(delta):
 
 
 func play_water_spalash_animation() -> void:
-	ball_sprite.hide()
 	water_splash_animation.show()
 	water_splash_animation.play("water_splash_animation")
-	
-	
+
+
+func delete_player() -> void:
+	ball_sprite.hide()
+	play_water_spalash_animation()
+	player_delete_timer.start()
+
+
+func _on_player_delete_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
--- Slingy Team/scenes/water/water.gd
+++ Slingy Team/scenes/water/water.gd
@@ -14,5 +14,5 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	water_splash_sound.play()
 	SignalManager.on_player_destroyed.emit()
+	water_splash_sound.play()
